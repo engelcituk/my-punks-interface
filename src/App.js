d0a9682af5132d00d54c0a8950e235331b68d625
cituk <programaciondyn3.mx@sandos.com>
2022-02-09 23:36:49 -0500

    Administrando la conexión a Metamask

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
+import { useEffect } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Home from './views/home';
+import Web3 from 'web3';
 
 function App() {
+  useEffect( () => {
+    if( window.ethereum ){
+      // window.ethereum.request({ method: 'eth_requestAccounts' }).then( console.log ) //version inicial
+      const web3 = new Web3(window.ethereum )
+      web3.eth.requestAccounts().then(console.log)
+    }
+  },[])
+
   return (
     <>
     <Routes>
