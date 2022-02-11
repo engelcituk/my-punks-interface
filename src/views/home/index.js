75c82835c9347c921afcde893637871614927bc9
cituk <programaciondyn3.mx@sandos.com>
2022-02-11 00:17:42 -0500

    Instanciando el contrato de MyPunks

--- src/views/home/index.js
+++ src/views/home/index.js
@@ -1,7 +1,28 @@
+import { useWeb3React } from '@web3-react/core';
+import { useCallback, useEffect, useState } from 'react';
+import useMyPunks  from '../../hooks/useMyPunks';
+
 const Home = () => {
+    const { active } = useWeb3React();
+    const [ maxSupply, setMaxSupply ] = useState();
+    const myPunks = useMyPunks();
+
+    const getMaxSupply = useCallback( async() =>{
+        if(myPunks){
+            const result = await myPunks.methods.maxSupply().call();
+            setMaxSupply(result)
+        }
+    }, [myPunks])
+
+    useEffect( ()=> {
+        getMaxSupply();
+    },[getMaxSupply])
+
+    if(!active) return 'Conecta tu Wallet';
+
     return (
         <>
-        <p>Hello world </p>
+        <p>maxSupply: {maxSupply}</p>
         </>
     );
 };
